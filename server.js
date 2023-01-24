d6360cb2041178be6dc577007b8ac8095805244d
tankmc31434 <621463024@crru.ac.th>
2023-01-24 20:21:21 +0700

    62463024

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-/*This is a Node.js server-side script that uses
- the Express and Socket.io libraries to create a real-time 
- WebRTC video chat application. */         
-
+/*นี่คือสคริปต์ฝั่งเซิร์ฟเวอร์ Node.js ที่ใช้
+ ไลบรารี Express และ Socket.io เพื่อสร้างแบบเรียลไทม์
+ แอปพลิเคชั่นวิดีโอแชท WebRTC */         
+ 
 
  /**
- * These lines import the Express and uuid libraries, 
- * and create an instance of the Express application and an HTTP server.
+ * บรรทัดเหล่านี้นำเข้าไลบรารี Express และ uuid, 
+ * และสร้างอินสแตนซ์ของแอปพลิเคชัน Express และเซิร์ฟเวอร์ HTTP.
  */
 const express = require("express");
 const app = express();
@@ -13,9 +13,9 @@ const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 
 /**
- * This line imports the ExpressPeerServer module from the peer library, 
- * and the next line creates an options object with the debug property 
- * set to true.
+ * บรรทัดนี้นำเข้าโมดูล ExpressPeerServer จากไลบรารีเพียร์, 
+ * และบรรทัดถัดไปสร้างออบเจกต์อ็อพชันที่มีคุณสมบัติการดีบัก 
+ * ตั้งค่าเป็นจริง.
  */
 const { ExpressPeerServer } = require("peer");
 const opinions = {
@@ -23,10 +23,10 @@ const opinions = {
 }
 
 /**
- * This line sets the view engine for the Express application to EJS, 
- * and the next line imports the Socket.io library 
- * and attaches it to the HTTP server. 
- * The cors property is used to allow connections from any origin.
+ * บรรทัดนี้ตั้งค่ากลไกการดูสำหรับแอปพลิเคชัน Express เป็น EJS, 
+ * และบรรทัดถัดไปนำเข้าไลบรารี Socket.io 
+ * และแนบกับเซิร์ฟเวอร์ HTTP. 
+ * คุณสมบัติ cors ใช้เพื่ออนุญาตการเชื่อมต่อจากต้นทางใดๆ.
  */
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
@@ -36,8 +36,8 @@ const io = require("socket.io")(server, {
 });
 
 /**
- * These lines mount the ExpressPeerServer middleware on the /peerjs route 
- * and serve the contents of the public directory as static assets.
+ * บรรทัดเหล่านี้ติดตั้งมิดเดิลแวร์ ExpressPeerServer บนเส้นทาง /peerjs 
+ * และให้บริการเนื้อหาของไดเร็กทอรีสาธารณะเป็นสินทรัพย์คงที่.
  */
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
@@ -45,9 +45,9 @@ app.use(express.static("public"));
 
 
 /**
- * This code creates a route for the root URL and 
- * redirects the user to a randomly generated room ID 
- * created by uuidv4()
+ * รหัสนี้สร้างเส้นทางสำหรับ URL รูทและ
+ * เปลี่ยนเส้นทางผู้ใช้ไปยังรหัสห้องที่สร้างขึ้นแบบสุ่ม 
+ * สร้างโดย uuidv4()
  */
 
 app.get("/", (req, res) => {
@@ -56,8 +56,8 @@ app.get("/", (req, res) => {
 
 
 /**
- * This code creates a route for URLs with a room parameter 
- * and renders the room.ejs view with the roomId parameter passed in.
+ * รหัสนี้สร้างเส้นทางสำหรับ URL ด้วยพารามิเตอร์ห้อง 
+ * และแสดงมุมมอง room.ejs ด้วยพารามิเตอร์ roomId ที่ส่งผ่านเข้ามา.
  */
 
 app.get("/:room", (req, res) => {
@@ -67,13 +67,13 @@ app.get("/:room", (req, res) => {
 
 
 /**
- * This code listens for a "connection" event from the client, 
- * and when a client connects it listens for a "join-room" event 
- * and joins the room with a certain roomId. 
- * It also emits an event "user-connected" with a certain userId 
- * after 1 sec. It also listens for a "message" event, 
- * and when a message is received, 
- * it emits the message and the userName to all clients in the room.
+ * รหัสนี้รับฟังเหตุการณ์ "การเชื่อมต่อ" จากลูกค้า, 
+ * และเมื่อไคลเอนต์เชื่อมต่อก็จะฟังเหตุการณ์ "เข้าร่วมห้อง" 
+ * และเข้าร่วมห้องด้วย roomId ที่แน่นอน
+ * นอกจากนี้ยังปล่อยเหตุการณ์ "เชื่อมต่อกับผู้ใช้" ด้วย userId บางอย่าง 
+ * หลังจาก 1 วินาที นอกจากนี้ยังฟังเหตุการณ์ "ข้อความ", 
+ * และเมื่อได้รับข้อความ, 
+ * มันส่งข้อความและชื่อผู้ใช้ไปยังไคลเอนต์ทั้งหมดในห้อง
  */
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
@@ -87,8 +87,8 @@ io.on("connection", (socket) => {
     });
 });
 
-/**This line starts the server and 
- * listens on the port specified in the 
- * PORT environment variable or port 3030 if it is not set. */
+/**บรรทัดนี้เริ่มต้นเซิร์ฟเวอร์และ
+ * ฟังพอร์ตที่ระบุใน 
+ * ตัวแปรสภาพแวดล้อม PORT หรือพอร์ต 3030 หากไม่ได้ตั้งค่าไว้ */
 server.listen(process.env.PORT || 3030);
 
